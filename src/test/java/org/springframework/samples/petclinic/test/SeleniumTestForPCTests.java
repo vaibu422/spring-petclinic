812560cdc01695f5954af338ed9e5310c2704944
unknown <kumarn12@MININT-USH7CCG.corp.emc.com>
2015-06-18 15:05:53 +0530

    no message

--- src/test/java/org/springframework/samples/petclinic/test/SeleniumTestForPCTests.java
+++ src/test/java/org/springframework/samples/petclinic/test/SeleniumTestForPCTests.java
@@ -10,7 +10,7 @@ import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.Select;
 
-public class SeleniumTestForPC {
+public class SeleniumTestForPCTests {
 
 	private WebDriver driver;
 	  private String baseUrl;
