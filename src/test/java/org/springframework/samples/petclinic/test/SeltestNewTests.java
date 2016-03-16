728dcd40e57d3fb608d044bed72d4fafa0114a0d
ballaa2 <Aruna.Balla@emc.com>
2016-03-16 16:35:41 +0530

    Update SeltestNewTests.java

--- src/test/java/org/springframework/samples/petclinic/test/SeltestNewTests.java
+++ src/test/java/org/springframework/samples/petclinic/test/SeltestNewTests.java
@@ -24,7 +24,7 @@ public class SeltestNewTests {
 
   @Before
   public void setUp() throws Exception {
-    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe"); 
+    System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver"); 
 // Initialize browser 
  driver=new ChromeDriver();
     //driver = new FirefoxDriver();
